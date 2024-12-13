be9f1dcb88660db3ac8ac6af295e7929ad20c2f8
restu <restupuja76@gmail.com>
2024-12-13 09:29:05 +0700

    entah

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,125 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@extends('layouts.temp')
+@section('content')
+<div class="bg-purple-900 absolute top-0 left-0 bg-gradient-to-b from-gray-900 via-gray-900 to-purple-800 bottom-0 leading-5 h-full w-full overflow-hidden">
+</div>
+<div class="relative min-h-screen flex flex-row justify-center bg-transparent rounded-3xl shadow-xl">
+    <div class="flex-col flex  self-center lg:px-14 sm:max-w-4xl xl:max-w-md  z-10">
+        <div class="text-white text-center px-12">
+            <h1 class="text-4xl font-semibold mb-4">Welcome!</h1>
+            <p class="text-sm opacity-75">Create your account and start your journey with us. Join our community and unlock exciting features.</p>
         </div>
+    </div>
+    
+    <div class="w-full lg:w-1/2 flex justify-center items-center">
+        <div class="p-12 bg-white mx-auto rounded-3xl w-96">
+            <div class="mb-7">
+                <h3 class="font-semibold text-2xl text-center text-gray-800">Sign Up</h3>
+                <p class="text-gray-400 text-center">Already have an account? 
+                    <a href="{{ route('login') }}" class="text-sm text-purple-700 hover:text-purple-700">Sign In</a>
+                </p>
+            </div>
+            
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="space-y-6">
+                    <div>
+                        <input class="w-full text-sm px-4 py-3 bg-gray-200 focus:bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-purple-400" 
+                               name="name" type="text" value="{{ old('name') }}" id="name" placeholder="Full Name" required>
+                        @error('name')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    
+                    <div>
+                        <input class="w-full text-sm px-4 py-3 bg-gray-200 focus:bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-purple-400" 
+                               name="email" type="email" value="{{ old('email') }}" id="email" placeholder="Email" required>
+                        @error('email')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    
+                    <div class="relative" x-data="{ show: true }">
+                        <input id="password" name="password" placeholder="Password" 
+                               :type="show ? 'password' : 'text'" 
+                               class="text-sm text-black px-4 py-3 rounded-lg w-full bg-gray-200 focus:bg-gray-100 border border-gray-200 focus:outline-none focus:border-purple-400" 
+                               required>
+                        @error('password')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                        @enderror
+                        
+                        <div class="flex items-center absolute inset-y-0 right-0 mr-3 text-sm leading-5">
+                            <svg @click="show = !show" :class="{'hidden': !show, 'block':show }"
+                                class="h-4 text-purple-700" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                viewbox="0 0 576 512">
+                                <path fill="currentColor"
+                                    d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
+                                </path>
+                            </svg>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            <svg @click="show = !show" :class="{'block': !show, 'hidden':show }"
+                                class="h-4 text-purple-700" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                viewbox="0 0 640 512">
+                                <path fill="currentColor"
+                                    d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <input id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" 
+                               type="password" 
+                               class="text-sm text-black px-4 py-3 rounded-lg w-full bg-gray-200 focus:bg-gray-100 border border-gray-200 focus:outline-none focus:border-purple-400" 
+                               required>
+                    </div>
+                    
+                    <div>
+                        <button type="submit" class="w-full flex justify-center text-center bg-purple-800 hover:bg-purple-700 text-gray-100 p-3 rounded-lg tracking-wide font-semibold cursor-pointer transition ease-in duration-500">
+                            Create Account
+                        </button>
+                    </div>
+                </div>
+            </form>
+            
+            <div class="flex items-center justify-center space-x-2 my-5">
+                <span class="h-px w-16 bg-gray-100"></span>
+                <span class="text-gray-300 font-normal">or</span>
+                <span class="h-px w-16 bg-gray-100"></span>
+            </div>
+            
+            <div class="flex justify-center gap-5 w-full">
+                <button type="submit" class="w-full flex items-center justify-center mb-6 md:mb-0 border border-gray-300 hover:border-gray-900 hover:bg-gray-900 text-sm text-gray-500 p-3 rounded-lg tracking-wide font-medium cursor-pointer transition ease-in duration-500">
+                    <svg class="w-4 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="#EA4335" d="M5.266 9.765A7.077 7.077 0 0 1 12 4.909c1.69 0 3.218.6 4.418 1.582L19.91 3C17.782 1.145 15.055 0 12 0 7.27 0 3.198 2.698 1.24 6.65l4.026 3.115Z"/>
+                        <path fill="#34A853" d="M16.04 18.013c-1.09.703-2.474 1.078-4.04 1.078a7.077 7.077 0 0 1-6.723-4.823l-4.04 3.067A11.965 11.965 0 0 0 12 24c2.933 0 5.735-1.043 7.834-3l-3.793-2.987Z"/>
+                        <path fill="#4A90E2" d="M19.834 21c2.195-2.048 3.62-5.096 3.62-9 0-.71-.109-1.473-.272-2.182H12v4.637h6.436c-.317 1.559-1.17 2.766-2.395 3.558L19.834 21Z"/>
+                        <path fill="#FBBC05" d="M5.277 14.268A7.12 7.12 0 0 1 4.909 12c0-.782.125-1.533.357-2.235L1.24 6.65A11.934 11.934 0 0 0 0 12c0 1.92.445 3.73 1.237 5.335l4.04-3.067Z"/>
+                    </svg>
+                    <span>Google</span>
+                </button>
+                
+                <button type="submit" class="w-full flex items-center justify-center mb-6 md:mb-0 border border-gray-300 hover:border-gray-900 hover:bg-gray-900 text-sm text-gray-500 p-3 rounded-lg tracking-wide font-medium cursor-pointer transition ease-in duration-500">
+                    <svg class="w-4 mr-2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M50 2.5c-58.892 1.725-64.898 84.363-7.46 95h14.92c57.451-10.647 51.419-93.281-7.46-95z" style="fill:#1877f2"/>
+                        <path d="M57.46 64.104h11.125l2.117-13.814H57.46v-8.965c0-3.779 1.85-7.463 7.781-7.463h6.021V22.101c-12.894-2.323-28.385-1.616-28.722 17.66V50.29H30.417v13.814H42.54V97.5h14.92V64.104z" style="fill:#f1f1f1"/>
+                    </svg>
+                    <span>Facebook</span>
+                </button>
+            </div>
+            
+            <div class="mt-7 text-center text-black text-xs">
+                <span>
+                    Copyright © 2021-2023 
+                    <a href="/" rel="" target="_blank" title="Codepen aji" class="text-purple-500 hover:text-purple-600">Around You</a>
+                </span>
+            </div>
         </div>
+    </div>
+</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<svg class="absolute bottom-0 left-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#fff" fill-opacity="1" d="M0,0L40,42.7C80,85,160,171,240,197.3C320,224,400,192,480,154.7C560,117,640,75,720,74.7C800,75,880,117,960,154.7C1040,192,1120,224,1200,213.3C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+</svg>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,34 @@ Route::middleware([ToSweetAlert::class])->group(function () {
     });
 });
 
+Route::get('/auth', function () {
+    return view('auth.auth');
+});
+
 
 Route::middleware(['auth', RoleMiddleware::class . ':Admin'])->group(function () {
-    Route::get('/homead', function () {
+    Route::get('/home', function () {
         return view('home');
     });
+    //provinsi
+    Route::get('/lokasi', [ProvinsiController::class, 'index'])->name('lokasi.index');
+    Route::get('/find-nearby', [ProvinsiController::class, 'findNearby'])->name('lokasi.index');
+
+
+    //kabupaten
+    Route::resource('kabupaten', KabupatenController::class);
+
+    //kecamatan
+    Route::resource('kecamatan', KecamatanController::class);
+
+    //desa
+    Route::resource('desa', DesaController::class);
+
+    //laporan
+    Route::resource('laporan', LaporanController::class);
+
+    //pinalti
+    Route::resource('pinalti', PinaltiController::class);
 });
 
     Route::middleware('auth')->group(function () {
@@ -38,60 +61,3 @@ Route::middleware(['auth', RoleMiddleware::class . ':Admin'])->group(function ()
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
-
-    //dashboard
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    //provinsi
-    Route::get('/lokasi', [ProvinsiController::class, 'index'])->name('lokasi.index');
-    Route::get('/find-nearby', [ProvinsiController::class, 'findNearby'])->name('lokasi.index');
-
-
-    //kabupaten
-    Route::get('/kabupaten', [KabupatenController::class, 'index'])->name('kabupaten.index');
-    Route::get('/kabupaten/create', [KabupatenController::class, 'create'])->name('kabupaten.create');
-    Route::post('/kabupaten', [KabupatenController::class, 'store'])->name('kabupaten.store');
-    Route::get('/kabupaten/{id}/edit', [KabupatenController::class, 'edit'])->name('kabupaten.edit');
-    Route::put('/kabupaten/{id}', [KabupatenController::class, 'update'])->name('kabupaten.update');
-    Route::delete('/kabupaten/{id}', [KabupatenController::class, 'destroy'])->name('kabupaten.destroy');
-
-    //kecamatan
-    Route::get('/kecamatan', [KecamatanController::class, 'index'])->name('kecamatan.index');
-    Route::get('/kecamatan/create', [KecamatanController::class, 'create'])->name('kecamatan.create');
-    Route::post('/kecamatan', [KecamatanController::class, 'store'])->name('kecamatan.store');
-    Route::get('/kecamatan/{id}/edit', [KecamatanController::class, 'edit'])->name('kecamatan.edit');
-    Route::put('/kecamatan/{id}', [KecamatanController::class, 'update'])->name('kecamatan.update');
-    Route::delete('/kecamatan/{id}', [KecamatanController::class, 'destroy'])->name('kecamatan.destroy');
-
-    //desa
-    Route::get('/desa', [DesaController::class, 'index'])->name('desa.index');
-    Route::get('/desa/create', [DesaController::class, 'create'])->name('desa.create');
-    Route::post('/desa', [DesaController::class, 'store'])->name('desa.store');
-    Route::get('/desa/{id}/edit', [DesaController::class, 'edit'])->name('desa.edit');
-    Route::put('/desa/{id}', [DesaController::class, 'update'])->name('desa.update');
-    Route::delete('/desa/{id}', [DesaController::class, 'destroy'])->name('desa.destroy');
-
-    //laporan
-    Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
-    Route::get('/laporan/create', [LaporanController::class, 'create'])->name('laporan.create');
-    Route::post('/laporan', [LaporanController::class, 'store'])->name('laporan.store');
-    Route::get('/laporan/{id}/edit', [LaporanController::class, 'edit'])->name('laporan.edit');
-    Route::put('/laporan/{id}', [LaporanController::class, 'update'])->name('laporan.update');
-    Route::delete('/laporan/{id}', [LaporanController::class, 'destroy'])->name('laporan.destroy');
-
-    //pinalti
-    Route::get('/pinalti', [PinaltiController::class, 'index'])->name('pinalti.index');
-    Route::get('/pinalti/create', [PinaltiController::class, 'create'])->name('pinalti.create');
-    Route::post('/pinalti', [PinaltiController::class, 'store'])->name('pinalti.store');
-    Route::get('/pinalti/{id}/edit', [PinaltiController::class, 'edit'])->name('pinalti.edit');
-    Route::put('/pinalti/{id}', [PinaltiController::class, 'update'])->name('pinalti.update');
-    Route::delete('/pinalti/{id}', [PinaltiController::class, 'destroy'])->name('pinalti.destroy');
-
-
-
-
-
-
-
-
-

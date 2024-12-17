327488a1dd143899106239b7cd7642a1f8e772a6
restu <restupuja76@gmail.com>
2024-12-17 12:39:06 +0700

    perubahan

--- resources/views/kabupaten/index.blade.php
+++ resources/views/kabupaten/index.blade.php
@@ -260,31 +260,4 @@
             </div>
         </div>
     </div>
-
-    @include('sweetalert::alert')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript">
-
-        // SweetAlert Notifikasi untuk session 'success'
-        @if (session('success'))
-            Swal.fire({
-                icon: 'success',
-                title: 'Sukses',
-                text: '{{ session('success') }}',
-            });
-        @endif
-
-        @if ($errors->any())
-            Swal.fire({
-                icon: 'error',
-                title: 'Gagal',
-                text: '{{ $errors->first() }}', // Menampilkan pesan error pertama
-            });
-        @endif
-    </script>
-
 @endsection
-

d0245747091b49ae6bc002999bc8624f36bfdd34
firm_ant <anay03240@gmail.com>
2025-02-04 14:26:51 +0700

    tambah laporan

--- resources/views/laporan/create.blade.php
+++ resources/views/laporan/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin')
+@extends('layouts.user')
 
 @section('title', 'Tambah Laporan')
 
@@ -22,15 +22,6 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="mb-3">
-                                <label for="report_id" class="form-label">Pelapor</label>
-                                <select name="report_id" id="report_id" class="form-select">
-                                    <option value="">-- Pilih Pelapor --</option>
-                                    @foreach ($users as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="mb-3">
                                 <label for="reported_id" class="form-label">Terlapor</label>
                                 <select name="reported_id" id="reported_id" class="form-select">
                                     <option value="">-- Pilih Terlapor --</option>
@@ -47,8 +38,11 @@
                                 <label for="alasan" class="form-label">Alasan</label>
                                 <textarea name="alasan" id="alasan" rows="4" class="form-control"></textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                            <a href="{{ route('laporan.index') }}" class="btn btn-secondary">Batal</a>
+                            <div class="modal-footer d-flex justify-content-between border-0 mt-2 mx-2">
+                                <button type="button" class="btn btn-danger" style="font-size: 14px; padding: 10px 30px;">Laporkan</button>
+                                <button type="button" class="btn btn-secondary"
+                                data-bs-dismiss="modal" style="font-size: 14px; padding: 10px 30px; background-color: #BEB9B9; color: white;">Batal</button>
+                            </div>
                         </div>
                     </div>
                 </form>
